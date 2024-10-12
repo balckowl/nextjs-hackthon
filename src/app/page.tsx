b4a699fa3de5e71eb468ac9764b1ab59d2f0cef6
balckowl <shiggyjr1492@gmail.com>
2024-10-12 18:55:19 +0900

    feat: ホームページの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div className="container mx-auto">
+      <Link href="/box-shadw" className="border">
+        Box Shadow Generator
+      </Link>
+      <Link href="/svg-waves" className="border">
+        SVG Waves Generator
+      </Link>
+    </div>
+  )
 }
