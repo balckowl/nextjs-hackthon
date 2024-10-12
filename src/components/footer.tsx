3826e54c67939ca1947f2d9a131af5a1d5c16cf6
難波隼人 <hayatonanba@gmail.com>
2024-10-12 18:37:19 +0900

    fix: fotterを修正

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <div className="border-t h-[30px]">
+    <div className="border-t h-[80px]">
       <div className="container h-full items-center">
-        <small>くじらチーム作</small>
+        <small>© progate whale 2024</small>
       </div>
     </div>
   )
