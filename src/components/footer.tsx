05a1f36f0374e68d17d0bc1da83140fbd7ca81fd
難波隼人 <hayatonanba@gmail.com>
2024-10-12 18:29:35 +0900

    feat: fotterを追加

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="border-t h-[30px]">
+      <div className="container h-full items-center">
+        <small>くじらチーム作</small>
+      </div>
+    </div>
+  )
+}
