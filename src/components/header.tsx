26020452bf4182a4e03a19378a57a846af310a1e
balckowl <shiggyjr1492@gmail.com>
2024-10-16 21:22:15 +0900

    style: 文字サイズを調整

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="h-[80px]">
       <div className="container mx-auto flex justify-between h-full items-center">
-        <Link href="/" className="text-[32px] font-bold">
+        <Link href="/" className="text-[30px] font-bold">
           <h1>Workspace</h1>
         </Link>
         <Button>ログイン</Button>
