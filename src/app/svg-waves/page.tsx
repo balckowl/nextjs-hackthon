f6c05b102e6c452452c6c65de297635d956dd238
balckowl <shiggyjr1492@gmail.com>
2024-10-17 00:04:41 +0900

    style: codeブロックのスタイルを変更しました。

--- src/app/svg-waves/page.tsx
+++ src/app/svg-waves/page.tsx
@@ -7,10 +7,12 @@ import { copyToClipboard } from '@/lib/copy-to-clipboard'
 import { getWavePath } from '@/lib/get-wave-path'
 import { useState } from 'react'
 import { RiClipboardLine } from 'react-icons/ri'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function Page() {
   const [color, setColor] = useState<string>('#ff5555')
-  const [opacity, setOpacity] = useState<number>(50)
+  const [opacity, setOpacity] = useState<number>(100)
   const [waveType, setWaveType] = useState<string>('sine')
   const waveList = ['smooth', 'sine', 'square']
   const [direction, setDirection] = useState<string>('bottom')
@@ -21,10 +23,9 @@ export default function Page() {
     <svg 
       viewBox="0 0 1440 590" 
       xmlns="http://www.w3.org/2000/svg" 
-      class="absolute ${direction}-0 left-0" transform="${direction === 'top' ? 'scale(1, -1)' : ''}
-    ">
+      class="transform="${direction === 'top' ? 'scale(1, -1)' : ''}">
       <path 
-        d="${getWavePath(waveType)}"
+        d="${getWavePath(waveType)}" 
         stroke="none" 
         stroke-width="0" 
         fill="${color}" 
@@ -36,7 +37,7 @@ export default function Page() {
 
   return (
     <div className="w-[75%] mx-auto min-h-[calc(100vh-80px)]">
-      <AlertPopup value={isCopySuccess} setValue={setIsCopySuccess}/>
+      <AlertPopup value={isCopySuccess} setValue={setIsCopySuccess} />
 
       <div className="grid grid-cols-2 gap-5 pt-[35px] mb-[35px]">
         <div className="h-[320px] rounded-2xl border-[3px] overflow-hidden relative">
@@ -58,9 +59,29 @@ export default function Page() {
           </svg>
         </div>
         <div className="h-[320px] rounded-2xl border-[3px] relative">
-          <button type="button" className="focus:outline-none absolute top-5 right-5 border-[2px] p-3 rounded-md" onClick={() =>copyToClipboard(svgCode, setIsCopySuccess)}>
+          <button
+            type="button"
+            className="focus:outline-none absolute top-5 right-5 border-[2px] p-3 rounded-md bg-white"
+            onClick={() => copyToClipboard(svgCode, setIsCopySuccess)}
+          >
             <RiClipboardLine color="#ededed" />
           </button>
+          <SyntaxHighlighter
+            customStyle={{
+              borderRadius: '16px',
+              overflow: 'scroll',
+              height: '100%',
+              margin: '0px',
+              padding: '0px',
+              display: 'flex',
+              alignItems: 'center',
+              fontSize: '13px',
+            }}
+            language="html"
+            style={oneLight}
+          >
+            {svgCode}
+          </SyntaxHighlighter>
         </div>
       </div>
 
