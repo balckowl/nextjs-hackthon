98b9591a085ede81ca9746bb1a79c60757219ac1
hir0 <katuyo1616.use@gmail.com>
2024-10-17 19:56:23 +0900

    feat: POSTできるようにしたよ(っ･ω･)っ

---
+++ src/app/api/box-shadow/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server"
+import { db } from "@/db"
+import { boxShadows } from "@/db/schema"
+import { auth } from "@/auth"
+
+const POST = async (req: NextRequest) =>{
+    const {isShared, offsetX, offsetY, blurRadius, spreadRadius, color, shadowColor} = await req.json()
+    
+    const session = await auth()
+
+    if(!session){
+        throw Error("認証してください")
+    }
+
+    const { user } = session
+
+    const userId = user?.id
+
+    await db.insert(boxShadows).values({ userId: userId as string, isShared, offsetX, offsetY, blurRadius, spreadRadius, color, shadowColor  })
+
+    return NextResponse.json({status: 201})
+}
+
+export {POST}
