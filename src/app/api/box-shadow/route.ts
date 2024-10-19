eaec7d8b62d74f8e4fa4aead3f853a7bd28c0eb4
CatsEleven <hatijolove@gmail.com>
2024-10-19 12:55:13 +0900

    feat: deleteできるよ_ﾛ(･ω･=) ｹｼｹｼ

--- src/app/api/box-shadow/route.ts
+++ src/app/api/box-shadow/route.ts
@@ -2,6 +2,7 @@ import  { type NextRequest, NextResponse } from "next/server"
 import { db } from "@/db"
 import { boxShadows } from "@/db/schema"
 import { auth } from "@/auth"
+import {eq} from "drizzle-orm"
 
 const POST = async (req: NextRequest) =>{
     const {title, isShared, offsetX, offsetY, blurRadius, spreadRadius, color, shadowColor} = await req.json()
@@ -21,4 +22,22 @@ const POST = async (req: NextRequest) =>{
     return NextResponse.json({status: 201})
 }
 
-export {POST}
+const DELETE = async (req: NextRequest) =>{
+    const {id} = await req.json()
+    
+    const session = await auth()
+
+    if(!session){
+        throw Error("認証してください")
+    }
+
+    const { user } = session
+
+    const userId = user?.id
+
+    await db.delete(boxShadows).where(eq(boxShadows.id, id));
+
+    return NextResponse.json({status: 204})
+}
+
+export {POST, DELETE}
