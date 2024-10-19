60963e180ca8fe1070b18b540c87dc7f3fa50e0a
hir0 <katuyo1616.use@gmail.com>
2024-10-19 11:47:09 +0900

    userのidでdataを取ってくるようにしたよ

--- src/app/data/box-shadow.ts
+++ src/app/data/box-shadow.ts
@@ -1,7 +1,19 @@
 import {db} from "@/db"
+import { boxShadows } from "@/db/schema"
+import { eq } from "drizzle-orm"
+
 
 export const getAllBoxshadows = async () => {
     const allBoxshadows = await db.query.boxShadows.findMany()
 
     return allBoxshadows
+}
+
+export const getAllBoxshadowsByUserid = async (userId: string) => {
+
+    const allUserBoxshadows = await db.query.boxShadows.findMany({
+        where: eq(boxShadows.userId, userId)
+      })
+
+    return allUserBoxshadows
 }
--- src/app/data/wave.ts
+++ src/app/data/wave.ts
@@ -1,7 +1,18 @@
 import {db} from "@/db"
+import { waves } from "@/db/schema"
+import { eq } from "drizzle-orm"
 
 export const getAllWaves = async () => {
-    const allwaves = await db.query.waves.findMany()
+    const allWaves = await db.query.waves.findMany()
 
-    return allwaves
+    return allWaves
+}
+
+export const getAllWavesByUserid = async (userId: string) => {
+
+    const allUserWaves = await db.query.waves.findMany({
+        where: eq(waves.userId, userId)
+    })
+
+    return allUserWaves
 }
