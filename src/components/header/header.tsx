67b16fa2cf2069d136fb93604250fa21a4bf3eaf
balckowl <shiggyjr1492@gmail.com>
2024-10-20 13:35:10 +0900

    feat: ヘッダーメニューを作成

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import MenuPopover from '../icon-popover/icon-popover'
 import Button from '../button/button'
+import { MdPeopleAlt, MdWaves } from 'react-icons/md'
+import { RiShadowFill } from 'react-icons/ri'
 
 export default async function Header() {
   const session = await auth()
@@ -13,25 +15,40 @@ export default async function Header() {
         <Link href="/" className="text-[30px] font-bold">
           <h1>Workspace</h1>
         </Link>
-        {!session && (
-          <form
-            action={async () => {
-              'use server'
-              await signIn('github')
-            }}
-          >
-            <Button>ログイン</Button>
-          </form>
-        )}
-        {session && <MenuPopover name={session.user?.name as string} image={session.user?.image as string}>
-          <Image
-            src={session.user?.image as string}
-            width={30}
-            height={30}
-            alt="アイコン"
-            className="border size-[42px] rounded-full cursor-pointer"
-          />
-        </MenuPopover>}
+        <div className='flex items-center gap-5'>
+          <div className='flex gap-3 border-r pr-[20px] items-center'>
+            <Link href="/box-shadow/new" className='flex items-center gap-1 border p-3 rounded-xl bg-white'>
+              <RiShadowFill />
+            </Link>
+            <Link href="/svg-waves/new" className='flex items-center gap-1 border p-3 rounded-xl bg-whtie'>
+              <MdWaves />
+            </Link>
+            <Link href="/community" className='flex items-center gap-1 border p-3 rounded-xl bg-white'>
+              <MdPeopleAlt />
+            </Link>
+          </div>
+          {!session && (
+            <form
+              action={async () => {
+                'use server'
+                await signIn('github')
+              }}
+            >
+              <Button>ログイン</Button>
+            </form>
+          )}
+          {session && (
+            <MenuPopover name={session.user?.name as string} image={session.user?.image as string}>
+              <Image
+                src={session.user?.image as string}
+                width={30}
+                height={30}
+                alt="アイコン"
+                className="border size-[42px] rounded-full cursor-pointer"
+              />
+            </MenuPopover>
+          )}
+        </div>
       </div>
     </div>
   )
