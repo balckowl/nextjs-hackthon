b7bbdc8adc22460c6be600e8da8a41bf56747ce0
meshiden-dot-jp <meganenasi@gmail.com>
2024-10-20 13:27:56 +0900

    style: トップページのデザイン変更

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -3,10 +3,12 @@ import WaveCard from '@/components/top/wave-card'
 
 export default function Home() {
   return (
-    <div className="w-[65%] mx-auto min-h-[calc(100vh-80px)]">
-      <h2 className="font-semibold text-[30px] text-center pt-[50px] pb-[60px]">
-        あたらしいコードを作りましょう
+    <div className="w-[65%] mx-auto min-h-[calc(100vh-80px)] overflow-hidden">
+      <img src="/logo/icon.svg" alt="Workspaceのアイコン" className='m-auto size-24 mt-12'/>
+      <h2 className="font-bold text-[58px] text-center pt-[24px]">
+        あたらしいコードを作りましょう。
       </h2>
+      <p　className="font-bold text-[20px] text-center pt-[8px] pb-[64px]">つくりたいエフェクトを選択してください。</p>
       <div className="grid grid-cols-2 gap-5">
         <BoxShadowCard />
         <WaveCard />
